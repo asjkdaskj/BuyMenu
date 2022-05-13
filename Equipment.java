bfef06b3cf7f0471f97ac2421974e1ec8a5cd015
gaostef <99352900+gaostef@users.noreply.github.com>
2022-05-12 21:15:49 -0400

    Create Equipment.java

---
+++ Equipment.java
@@ -0,0 +1,19 @@
+/**
+ * name: Stefany 
+ * date 2022-04-29 
+ * description: equipment
+ */
+public class Equipment extends Item {
+
+	public Equipment(double cost, String name, boolean isBought) {
+		super(cost, name, isBought);
+	}
+
+	public void useKit() {
+		// if (kit.isBought = true){
+		// defuse = 10; }
+		// else {
+		// defuse = 5;
+		// }
+	}
+}
